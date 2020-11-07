d3cfa0cd035417b7ed722dd3fef496426c04e11c
Saiful-Islam-Sakib <sakib19041996@gmail.com>
2020-11-08 03:38:15 +0600

    hi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      {/* header */}
+      {/* body */}
       <h1> hello </h1>
-      
+      {/* footer */}
     </div>
   );
 }
