ff1f09f53654ebfbed8f019691957dc423f69041
Saiful-Islam-Sakib <sakib19041996@gmail.com>
2020-11-13 03:59:20 +0600

    -

--- frontend/src/Components/HomePageBody.js
+++ frontend/src/Components/HomePageBody.js
@@ -6,10 +6,12 @@ import Container from "@material-ui/core/Container";
 
 export default function HomePageBody() {
     return (
-        <Container maxWidth="md">
-            <OfferSlider></OfferSlider>
-            <TopProducts></TopProducts>
-            <TopDeals></TopDeals>
-        </Container>
+        <div>
+            <Container maxWidth="md">
+                <OfferSlider></OfferSlider>
+                <TopProducts></TopProducts>
+                <TopDeals></TopDeals>
+            </Container>
+        </div>
     );
 }
