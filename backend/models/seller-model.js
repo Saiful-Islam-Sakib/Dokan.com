7c4ac7be094c8dd0b6c8c53246f03cec75671320
Md Junaeid Bhuiyan <mdjunaeid98@gmail.com>
2020-11-19 04:35:59 +0600

    Seller model added and seller controller integrated with mongoDB

---
+++ backend/models/seller-model.js
@@ -0,0 +1,27 @@
+const mongo  = require('mongoose');
+
+const sellerSchema = new mongo.Schema({
+    //s_id : {type:String,required:true},
+    v_f_name : {type:String,required:true},
+    v_l_name : {type:String,required:true},
+    email : {type:String,required:true},
+    phone : {type:String,required:true},
+    trade_lic_no : {type:String,required:true},
+    birthday : {type:String,required:true},
+    v_city : {type:String,required:true},
+    v_area : {type:String,required:true},
+    v_address : {type:String,required:true},
+    nid : {type:String,required:true},
+    password : {type:String,required:true},	
+    b_acc: {type:String,required:true}, 
+    b_acc_no: {type:String,required:true},
+    bank : {type:String,required:true},
+    branch : {type:String,required:true},
+    sh_name: {type:String,required:true},
+    sh_city : {type:String,required:true},
+    sh_area : {type:String,required:true},
+    sh_place : {type:String,required:true},
+    sh_area_pc : {type:String,required:true}
+});
+
+module.exports = new mongo.model('Seller',sellerSchema);
