dddaf4a1d74b08e1ce87fb97f226d6e6954b8d98
Md Junaeid Bhuiyan <mdjunaeid98@gmail.com>
2020-11-28 15:59:30 +0600

    Silly mistake modified

--- backend/controllers/order-controllers.js
+++ backend/controllers/order-controllers.js
@@ -64,8 +64,8 @@ const createNewOrder = async (req,res,next) =>{
             const erro = new httpError('Could not find product',500);
             return next(erro);
         }
-        const pname = productexist.name;
-        const createdorder = new order ({p_id : p_id[i], p_name : pname[i],
+        let pname = productexist.name;
+        const createdorder = new order ({p_id : p_id[i], p_name : pname,
             quantity : quantity[i], total_amount : total_amount[i],
             c_id,
             order_confirmation,order_delivered,
