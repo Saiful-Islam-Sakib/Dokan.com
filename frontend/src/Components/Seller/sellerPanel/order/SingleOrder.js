bf374274a22f2a6585445f1742b47630e10e1cf5
Saiful-Islam-Sakib <sakib19041996@gmail.com>
2020-12-01 00:54:34 +0600

    +

---
+++ frontend/src/Components/Seller/sellerPanel/order/SingleOrder.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+
+import "./Order.css";
+
+export default function SingleOrder() {
+    const dispatch = useDispatch();
+
+    const handleOrderAccept = () => {
+        dispatch({
+            type: "ACCEPT_ORDER",
+            //orderId: ,
+        });
+    };
+
+    const handleOrderReject = () => {
+        dispatch({
+            type: "REJECT_ORDER",
+            //orderId: ,
+        });
+    };
+
+    return (
+        <li class="order-item">
+            <img
+                src="https://sindabad.com/media/catalog/product/cache/15869ca78e92508fe510db2b345701fc/o/n/onion_peyaj_local_3.jpg"
+                alt="peyaj"
+            />
+            <div class="order-details">
+                <div class="order-date">21-12-2020</div>
+                <div class="order-name">ata moyd asuji</div>
+                <div class="order-price">Address: omuk tomuk</div>
+                <div class="order-price">Price: 10 tk</div>
+                <div class="order-amount">2x</div>
+                <div class="order-totalPrice">Total Price: 20 tk</div>
+                <div class="product-order-buttons">
+                    <button
+                        class="accept-order-button"
+                        onClick={handleOrderAccept}
+                    >
+                        ACCEPT
+                    </button>
+                    <button
+                        class="cancel-order-button"
+                        onClick={handleOrderReject}
+                    >
+                        DECLINE
+                    </button>
+                </div>
+            </div>
+        </li>
+    );
+}
--- frontend/src/Redux/Reducer/seller.reducers.js
+++ frontend/src/Redux/Reducer/seller.reducers.js
@@ -46,6 +46,35 @@ export default (state = initialState, action) => {
                 //products: ,
             };
         case "LOAD_MY_ORDERS":
+            //sellerId = action.sellerId;
+
+            (async () => {
+                try {
+                    // load orders functionality here .........................................................................................
+                } catch (error) {}
+            })();
+
+            return {
+                ...state,
+                //orders: ,
+            };
+        case "ACCEPT_ORDER":
+            //let orderId = action.orderId;
+
+            (async () => {
+                try {
+                    // accept orders functionality here .........................................................................................
+                } catch (error) {}
+            })();
+            return {
+                ...state,
+            };
+        case "REJECT_ORDER":
+            (async () => {
+                try {
+                    // reject orders functionality here .........................................................................................
+                } catch (error) {}
+            })();
             return {
                 ...state,
             };
