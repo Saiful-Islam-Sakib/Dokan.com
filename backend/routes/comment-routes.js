8a6bc885d8f4d138265f9e98b786d107d130c6b5
Md Junaeid Bhuiyan <mdjunaeid98@gmail.com>
2020-12-03 05:28:41 +0600

    Report validation added in backend

--- backend/routes/comment-routes.js
+++ backend/routes/comment-routes.js
@@ -10,6 +10,6 @@ router.post('/report',commentController.report);
 
 //router.get('/comment/fun',commentController.fun);
 
-router.post('/test',commentController.test);
+router.post('/test',[check('email').not().isEmpty(),check('body').not().isEmpty(),],commentController.test);
 
 module.exports = router;
